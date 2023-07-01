c783c21b6bcb623513928b9999a78e75290091f4
Nathan G Bornstein <grayfox_801@hotmail.com>
2023-07-01 10:12:32 -0500

    Add bulk of already coded project outside of Git

---
+++ script.js
@@ -0,0 +1,16 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    if (count === 0) return
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
